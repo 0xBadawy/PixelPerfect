28db11345233637a94e35b0740dd2cb7ef03cfb6
Moahemd-basawy <mohamedbadawysayed02@gmail.com>
2024-01-23 11:12:28 +0200

    commit

--- Code File/MainForm.py
+++ Code File/MainForm.py
@@ -42,6 +42,8 @@ button2 = customtkinter.CTkButton(app, text="Open ", command=Create_image)
 # =================== widgets ===================
 button.grid(row=0, column=0, padx=20, pady=20)
 button2.grid(row=2, column=0, padx=30, pady=30)
+button.grid(row=0, column=0, padx=20, pady=20)
+button2.grid(row=2, column=0, padx=30, pady=30)
 
 
 
