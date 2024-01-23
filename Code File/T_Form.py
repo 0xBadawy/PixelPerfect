436cc8d11dcbfe5d3cd5f243e58881b3c97a89f5
Moahemd-basawy <mohamedbadawysayed02@gmail.com>
2024-01-23 09:41:08 +0200

    Edits file

--- Code File/T_Form.py
+++ Code File/T_Form.py
@@ -1,29 +1,20 @@
 import customtkinter
 
+# =================== window setup ===================
+customtkinter.set_appearance_mode("dark") # "dark" or "light"
 app = customtkinter.CTk()
 app.title("CTkSwitch Demo")
-app.geometry("500x500")
+app.geometry("1600x800")
 
 
-
-val=0
-
-def switch_event():
-    global val
-    if(val % 2==0):
-        customtkinter.set_appearance_mode("dark")
-        val+=1
-    else:
-        customtkinter.set_appearance_mode("light")
-        val+=1
-    print(val)
-
-switch_var = customtkinter.StringVar(value="on")
-switch = customtkinter.CTkSwitch(app, text="CTkSwitch", command=switch_event,
-    variable=switch_var, onvalue="on", offvalue="off")
+def button_callback():
+    print(checkbox_1.get())
 
 
-switch.grid(row=5, column=2, padx=40, pady=40)
+# =================== main ===================
 
+checkbox_1 = customtkinter.CTkCheckBox(app, text="checkbox 1"  ,command=button_callback)
+print(checkbox_1.get())
+checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
 
 app.mainloop()
