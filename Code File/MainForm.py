2bbf6f370c13f3ab5b1a491bf2b6310120ee7bdc
Moahemd-basawy <mohamedbadawysayed02@gmail.com>
2024-02-03 10:35:04 +0200

    N

--- Code File/MainForm.py
+++ Code File/MainForm.py
@@ -25,27 +25,20 @@ folder_path = ""
 first_image=""
 font_color = ""
 save_in_same_folder = True
-
-def PP():
-    print(folder_path)
-    print(first_image)
+image_original_R=""
+logo_Path="Test_images\Frame.png"
 
     
+
+# =================== images ===================
     
 def get_first_image_path():
     global folder_path , first_image
     for file_name in os.listdir(folder_path):
         if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
-            VV= os.path.join(folder_path, file_name)
-            print(VV)
-            first_image=VV
-            #canvas.bind('<Configure>',show_full_image)
-
+            first_image=os.path.join(folder_path, file_name)
             return      
     return None
-
-
-# =================== images ===================
 
 def fill_image(event):
     global resized_tk
@@ -61,10 +54,9 @@ def fill_image(event):
     resized_tk = ImageTk.PhotoImage(resized_image)
     canvas.create_image(int(event.width / 2),int(event.height / 2),anchor = 'center',image = resized_tk)
 
-
-def show_full_image(event):
-    IOI()
-    global resized_tk, image_original_R, canvas
+def show_full_image(event,image_original_R,canvas):
+    Place_image()
+    global resized_tk
 
     canvas_ratio = event.width / event.height
     image_ratio = image_original_R.size[0] / image_original_R.size[1]
@@ -86,7 +78,6 @@ def show_full_image(event):
         image=resized_tk
     )
     
-
 # =================== functions ===================
 def Select_Folder():
     folderpath = ctk.filedialog.askdirectory()
@@ -127,11 +118,6 @@ def TextInfo():
 app.columnconfigure((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15),weight=1,uniform='a')
 app.rowconfigure((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14),weight=1,uniform='a')
 
-
-
-
-
-
 # =================== main Frames ===================
 TopBar_Frame=ctk.CTkFrame(app,fg_color="#fff")
 TopBar_Frame.grid(row=0,column=0,columnspan=16,stick='nsew',pady=5,padx=5)
@@ -155,29 +141,18 @@ ButtonBar_Frame.grid(row=14,column=2,rowspan=1,columnspan=12,stick='nsew',pady=5
 
 # =================== MiddleBar ===================
 
-image_original_R=""
-if first_image == "":
-    image_original_R = Image.open('Test_images\\SSR.png')
-else:
-    image_original_R = Image.open(first_image)
-        
+def Place_image():
+    global first_image,image_original_R        
+    if first_image == "":image_original_R = Image.open('Test_images\\SSR.png')
+    else:image_original_R = Image.open(first_image)   
+              
+Place_image()
 
-def IOI():
-    global first_image,image_original_R
-    #print("Done : ",first_image)
-        
-    if first_image == "":
-        image_original_R = Image.open('Test_images\\SSR.png')
-    else:
-        image_original_R = Image.open(first_image)
-            
-        
-#image_original_R = Image.open('Test_images\\SSR.png') 
 Image_tk_R = ImageTk.PhotoImage(image_original_R)
-
 canvas=tk.Canvas(MiddleBar_Frame,background="#111",highlightthickness=0,relief='flat',bd=0)
 canvas.pack(fill='both',expand=True)
-canvas.bind('<Configure>',show_full_image)
+canvas.bind('<Configure>',lambda event: show_full_image(event,image_original_R,canvas))
+
 
 # =================== ButtonBar ===================
 
@@ -187,20 +162,28 @@ canvas.bind('<Configure>',show_full_image)
 ButtonBar_Frame.columnconfigure((0,1,2,3,4),weight=1,uniform='a')
 ButtonBar_Frame.rowconfigure(0,weight=1,uniform='a')
 
-
-
 SelectFolder_Button = ctk.CTkButton(ButtonBar_Frame, text="Select Folder", command=Select_Folder,font=("cairo", 20))
 SelectFolder_Button.grid(row=0, column=2, padx=5, pady=5)
 
 
-SelectFolder_ButtonIII= ctk.CTkButton(ButtonBar_Frame, text=" Folder", command=get_first_image_path,font=("cairo", 20))
-SelectFolder_ButtonIII.grid(row=0, column=1, padx=5, pady=5)
+
+# =================== RightBar ===================
+RightBar_Frame.columnconfigure(0,weight=1,uniform='a')
+RightBar_Frame.rowconfigure((0,1,2,3,4),weight=1,uniform='a')
 
 
+Logo_Frame=ctk.CTkFrame(RightBar_Frame,fg_color="#FC222E")
+Logo_Frame.grid(row=0, column=0,stick='nsew',pady=5,padx=5)
 
-SelectFolder_ButtonII= ctk.CTkButton(ButtonBar_Frame, text=" 8", command=PP,font=("cairo", 20))
-SelectFolder_ButtonII.grid(row=0, column=4, padx=5, pady=5)
+SelectLogo_Button = ctk.CTkButton(RightBar_Frame, text="Select Logo", command=Select_Folder,font=("cairo", 20))
+SelectLogo_Button.grid(row=1, column=0,sticky='n')
 
+
+# Image_tk_RLogo = ImageTk.PhotoImage(image_original_R)
+# Logo_Canvas=tk.Canvas(Logo_Frame,background="#111",highlightthickness=0,relief='flat',bd=0)
+
+# Logo_Canvas.pack(fill='both',expand=True)
+# Logo_Canvas.bind('<Configure>',lambda event: show_full_image(event,logo_Path,Logo_Canvas))
 
 
 
@@ -208,4 +191,6 @@ SelectFolder_ButtonII.grid(row=0, column=4, padx=5, pady=5)
 app.mainloop()
 
 
+
+# Disply image seqace =>   Select_Folder() -> get_first_image_path() -> Place_image()
 
