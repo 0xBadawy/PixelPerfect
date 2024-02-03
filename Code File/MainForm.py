b0974292ee67e9c4a81d8151ceec7a65289da6f7
Moahemd-basawy <mohamedbadawysayed02@gmail.com>
2024-02-03 21:28:21 +0200

    Update

--- Code File/MainForm.py
+++ Code File/MainForm.py
@@ -213,10 +213,6 @@ def P():
     print("Logo_Frame width",Logo_Frame.winfo_width(),"Logo_Frame height",Logo_Frame.winfo_height())
     
 
-# canvas_Logo.pack(fill='both', expand=True)
-# canvas_Logo.create_image(0, 0, anchor="nw", image=Image_Logo)
-    
-
 # Image_Logo = ImageTk.PhotoImage(logo_Path)
 # canvas_Logo=tk.Canvas(Logo_Frame,
 #                  width=min(1057,logo_Path.size[0]),
